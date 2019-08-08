5f13f8778dc6611bee51073647b827c87da89915
Pawi133 <pawi133@gmail.com>
2019-08-08 15:11:46 +0200

    Pointing to correct file

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var Typer={
 	addText:function(key){
 
 		if(key.keyCode==18){
-			Typer.accessCount++; 
+			Typer.accessCount++;
 
 			if(Typer.accessCount>=3){
 				Typer.makeAccess();
@@ -98,7 +98,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="CodeNerve.txt";
+Typer.file="motd.txt";
 Typer.init();
 
 var timer = setInterval("t();", 30);
